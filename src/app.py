6bec2594e11e6a4eed29ac53fab14d1746a99e26
Song Yi Wei <ywsong@aceinna.com>
2020-02-21 14:28:17 +0800

    travis

---
+++ src/app.py
@@ -0,0 +1,11 @@
+from math import fabs, ceil
+
+def math_fabs(x):
+    return fabs(x)
+
+def math_ceil(x):
+    return ceil(x)
+
+if __name__ == '__main__':
+    print(math_fabs(-1.2))
+    print(math_ceil(-2.3))
---
+++ tests/test_simple.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+import sys
+sys.path.append('.')
+import pytest
+from src.app import math_fabs, math_ceil
+
+def test_math_fabs():
+    assert math_fabs(-1.2) == 1.2
+    assert math_fabs(0) == 0
+    assert math_fabs(2.4) == 2.4
+
+def test_math_ceil():
+    assert math_ceil(-1.2) == -1
+    assert math_ceil(0) == 0
+    assert math_ceil(2.4) == 3
